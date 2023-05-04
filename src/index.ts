83f479915f010b0f40b99363ea069f1d60282251
lfernandes00 <luis_santos_fernandes@hotmail.com>
2023-05-04 23:01:25 +0100

    Project setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("testesssssssssss");
+require("dotenv").config();
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.listen(port, () => console.log(`Server running on Port: ${port}`));
