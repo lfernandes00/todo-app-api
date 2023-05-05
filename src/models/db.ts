ffa7e4324079f909cbc92715d960e922a313ba1c
lfernandes00 <luis_santos_fernandes@hotmail.com>
2023-05-06 00:21:26 +0100

    config database creadentials

---
+++ src/models/db.ts
@@ -0,0 +1,27 @@
+import { config } from "../config/db.config";
+import { Sequelize, DataTypes } from "sequelize";
+
+const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+  host: config.HOST,
+  dialect: "mysql",
+  pool: {
+    max: config.pool.max,
+    min: config.pool.min,
+    acquire: config.pool.acquire,
+    idle: config.pool.idle,
+  },
+  port: 3306,
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Connection has been established successfully!"))
+  .catch((err) => console.log("Unable to connect to the database: ", err));
+
+const db: any = {};
+db.sequelize = sequelize;
+
+db.user = require("./users.model")(sequelize, DataTypes);
+db.project = require("./projects.model")(sequelize, DataTypes);
+
+export = db;
