f3a100d58a76f047caafb8a7a246475fbcb2aba5
lfernandes00 <luis_santos_fernandes@hotmail.com>
2023-05-07 23:11:50 +0100

    import and use projectRouter

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ require("dotenv").config();
 import express from "express";
 
 import userRouter from "./routes/users.routes";
+import projectRouter from "./routes/projects.routes";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use("/users", userRouter);
+app.use("/projects", projectRouter);
 
 app.listen(port, () => console.log(`Server running on Port: ${port}`));
