08a2eea0ee803f339e7e69d35b0e0f9dba1b4b26
lfernandes00 <luis_santos_fernandes@hotmail.com>
2023-05-08 23:08:28 +0100

    extend Request interface to add loggedUserId

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,14 @@ import express from "express";
 import userRouter from "./routes/users.routes";
 import projectRouter from "./routes/projects.routes";
 
+declare global {
+  namespace Express {
+    export interface Request {
+      loggedUserId: number;
+    }
+  }
+}
+
 const app = express();
 const port = process.env.PORT || 8000;
 
