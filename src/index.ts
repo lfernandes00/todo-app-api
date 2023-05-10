f3f4a042efe2ba6d0f1f48cc3df9b86dcb262b1c
lfernandes00 <luis_santos_fernandes@hotmail.com>
2023-05-10 23:15:18 +0100

    import task router

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 
 import userRouter from "./routes/users.routes";
 import projectRouter from "./routes/projects.routes";
+import taskRouter from "./routes/tasks.routes";
 
 declare global {
   namespace Express {
@@ -18,5 +19,6 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/projects", projectRouter);
+app.use("/tasks", taskRouter);
 
 app.listen(port, () => console.log(`Server running on Port: ${port}`));
